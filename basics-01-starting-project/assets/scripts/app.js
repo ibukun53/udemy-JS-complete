30f8a59c0b1d2fc823dffe008bf6581b0071b1ed
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-01-05 09:34:45 +0100

    Add description features

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,7 +2,8 @@
 const defaultReault = 0;
 let currentResult = defaultReault;
 function add() {
+  const calDescription = `${currentResult} + ${userInput.value}`;
   currentResult = currentResult + +userInput.value;
-  outputResult(currentResult, '');
+  outputResult(currentResult, calDescription);
 }
 addBtn.addEventListener('click', add);

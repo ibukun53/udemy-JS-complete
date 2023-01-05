78c7e22d555326c7f59dae3906fff29863886e09
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-01-05 09:49:42 +0100

    Chanage the function to arrow function and create a separate for userNumberInput

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
 const defaultReault = 0;
 let currentResult = defaultReault;
-function add() {
-  const enteredNumber = +userInput.value;
+const enterUserNumberInput = () => +userInput.value;
+const add = () => {
+  const enteredNumber = enterUserNumberInput();
   const calDescription = `${currentResult} + ${enteredNumber}`;
   currentResult = currentResult + +enteredNumber;
   outputResult(currentResult, calDescription);
-}
+};
 addBtn.addEventListener('click', add);

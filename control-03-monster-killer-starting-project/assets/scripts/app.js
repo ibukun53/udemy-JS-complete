da5c10c748331ed3e009ef2d423897963ecffa8b
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-01-07 13:01:16 +0100

    Add heal functionality

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
-const attackValue = 10;
-const strongAttackValue = 17;
-const monsterAttackValue = 14;
+const attack_Value = 10;
+const strong_Attack_Value = 17;
+const monster_Attack_Value = 14;
+const heal_Value = 20;
 
 let chosenMaxValue = 100;
 let currentMonsterHealth = chosenMaxValue;
@@ -8,17 +9,8 @@ let curentPlayerHealth = chosenMaxValue;
 
 adjustHealthBars(chosenMaxValue);
 
-const attackMonster =(mode) =>{
- let maxDamage;
- if(mode === 'ATTACK'){
-    maxDamage = attackValue;
- } else if(mode === 'STRONG-ATTACK'){
-  maxDamage = strongAttackValue;
- }
-
- const damage = dealMonsterDamage( maxDamage);
-  currentMonsterHealth -= damage;
-  const PlayerDamage = dealPlayerDamage(monsterAttackValue);
+const endRound = () => {
+  const PlayerDamage = dealPlayerDamage(monster_Attack_Value);
   curentPlayerHealth -= PlayerDamage;
   if (currentMonsterHealth <= 0 && curentPlayerHealth > 0) {
     alert('you win');
@@ -27,14 +19,39 @@ const attackMonster =(mode) =>{
   } else if (curentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('you have a draw');
   }
-}
+};
+const attackMonster = (mode) => {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = attack_Value;
+  } else if (mode === 'STRONG-ATTACK') {
+    maxDamage = strong_Attack_Value;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
+};
 
 const attackHandler = () => {
-    attackMonster('ATTACK');
+  attackMonster('ATTACK');
 };
 
 const strongAttackHandler = () => {
-    attackMonster('STRONG-ATTACK');
+  attackMonster('STRONG-ATTACK');
+};
+
+const healPlayerHandler = () => {
+  let healValue;
+  if (curentPlayerHealth >= chosenMaxValue - heal_Value) {
+    alert("you can't heal to more than max initaila health.  ");
+    healValue = chosenMaxValue - curentPlayerHealth;
+  } else {
+    healValue = heal_Value;
+  }
+  increasePlayerHealth(heal_Value);
+  curentPlayerHealth += heal_Value;
+  endRound();
 };
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);

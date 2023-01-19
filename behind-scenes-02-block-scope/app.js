6c2b21f42d9e258177c611a22d5ea608e86806db
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-01-19 11:22:43 +0100

    Add and check behind scene code

---
+++ behind-scenes-02-block-scope/app.js
@@ -0,0 +1,18 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-undef */
+let name = 'Max';
+
+if (name === 'Max') {
+  let hobbies = ['Sports', 'Cooking'];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();

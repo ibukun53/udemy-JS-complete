003199f3c94a438c7723f262571327eb3bda8c53
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-09-26 12:16:51 +0100

    Implementing the user logic

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,9 +1,29 @@
 const startGameBtn = document.getElementById('start-game-btn');
-// const start = function() {
-// console.log('game is starting .....');
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
+let gameIsRunning = false;
 
-startGameBtn.addEventListener('click', function() = {
+const getPlayerChoice = () => {
+  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}`, '').toUpperCase();
+  if (selection !== ROCK
+ && selection !== PAPER
+ && selection !== SCISSORS
+  ) {
+    alert(`invalid choices! We chose ${DEFAULT_USER_CHOICE} for you`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener('click', () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('game is starting .....');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });

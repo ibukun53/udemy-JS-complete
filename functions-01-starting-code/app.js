87590b60909d12e92af8e62febd684197962fb31
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-09-26 13:03:25 +0100

    implementing the computer logic and introducing arrow function

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -3,6 +3,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -18,6 +21,23 @@ const getPlayerChoice = () => {
   return selection;
 };
 
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } if (randomValue > 0.67) {
+    return PAPER;
+  }
+  return SCISSORS;
+};
+
+const determineWinner = (cChoice, pChoice) => (cChoice === pChoice ? RESULT_DRAW
+  : (cChoice === ROCK && pChoice === PAPER)
+    || (cChoice === PAPER && pChoice === SCISSORS)
+   || (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULLT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS);
+
 startGameBtn.addEventListener('click', () => {
   if (gameIsRunning) {
     return;
@@ -25,5 +45,7 @@ startGameBtn.addEventListener('click', () => {
   gameIsRunning = true;
   console.log('game is starting .....');
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerSelection = getComputerChoice();
+  const winner = determineWinner(computerSelection, playerSelection);
+  console.log(winner);
 });

383118228dfa851945f24dee77a5b1eb0f9c7e16
Blessing Olusanya <debbyblessing1234@gmail.com>
2023-09-26 11:47:29 +0100

    changing the function to anonymous function

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,5 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn');
-const start = function startGame() {
+// const start = function() {
+// console.log('game is starting .....');
+// };
+
+
+startGameBtn.addEventListener('click', function() = {
   console.log('game is starting .....');
-};
-startGameBtn.addEventListener('click', start);
+});
